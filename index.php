560d62280d8a1d7b95c4381b54cee03180e8013f
xaz89 <dcg.jip@hotmail.com>
2017-12-22 17:04:16 -0600

    Sección cofradia

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="MBU Zona Centro | Ciudad de México, Hidalgo, Estado de México">
+	<meta name="author" content="Jazmín Isol Prado">
+	<title>MBU | Zona Centro</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Lato|Quicksand" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
+
+
+</head>
+<body>
+	<div class="forma">
+
+		<?php include(menu.php); ?>
+
+
+
+		<div class="contenedor centrado">
+			
+			<div class="forma">	
+			    <div class="back-video">
+			    	
+
+			      	<iframe src="https://www.youtube.com/embed/b5EtWla_X3M?&autoplay=1&amp;rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+			      	<!-- <iframe src="https://www.youtube.com/embed/Z9ubClnG0Ho?&amp;autoplay=1&amp;rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>  -->
+			    </div>
+			    <div class="forma">
+			    	<div class="back-image"><img src="img/logo-mbu.svg" width="50%" class="centrado" alt="">
+			    		<div class="forma front-image txt-center">
+							
+							<h1>MBU | Zona Centro</h1>
+
+						</div>
+			    	</div>
+			    </div>
+			    <div class="forma padding-b50 padding-t50">
+			    	<div class="contenedor front-video">
+						<h1>MBU | Zona Centro</h1>
+						<div class="forma">
+							<div class="front-video">		
+								<p><img src="img/icono1.png" class="centrado" alt="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ut, error quod qui, nesciunt dignissimos obcaecati, perferendis deserunt, recusandae illo aliquam? </p>
+								<p><img src="img/icono2.png" class="centrado" alt="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ut, error quod qui, nesciunt dignissimos obcaecati, perferendis deserunt, recusandae illo aliquam? </p>
+								<p><img src="img/icono3.png" class="centrado" alt="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ut, error quod qui, nesciunt dignissimos obcaecati, perferendis deserunt, recusandae illo aliquam? </p>
+								
+							</div>
+			    		</div>
+			    	</div>
+		    	</div>
+			</div>
+		</div>
+		
+		<div id="nosotras" class="forma padding-b50 padding-t50"> 
+			<div class="contenedor centrado">
+				<h1 class="secciones">Nosotras</h1>
+				<div class="cin">
+					<div class="forma">
+						<div class="nov centrado-devices">
+							
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto placeat et ipsam ullam, fugiat consectetur id. Inventore tenetur, ratione harum. Esse aut pariatur quisquam quas deserunt temporibus, aperiam itaque laudantium.</p>
+							<p>Omnis dolorum voluptatibus, non veniam error ipsa debitis harum aspernatur qui incidunt dicta.</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto placeat et ipsam ullam, fugiat consectetur id. Inventore tenetur, ratione harum. Esse aut pariatur quisquam quas deserunt temporibus, aperiam itaque laudantium.</p>
+							
+							<button class="boton centrado-devices">Ver más</button>
+						</div>
+					</div>
+				</div>
+				<div class="cin">
+					<img src="img/mbu1.jpg" width="100%"></img>
+				</div>
+			</div>
+		</div>
+
+		<div id="galeria" class="forma padding-b50"> 
+				<div class="contenedor centrado">
+
+
+					<h1>Galería</h1>
+					<!-- Inicia Galería Parallax
+					<div class="p1"></div>
+					<div class="p2"></div>
+					<div class="p6"></div>
+					Termina Galería Parallax -->
+					<div class="forma ">
+						<div class="forma margin-b10 ">
+							<div class="trein centrado-devices">
+								<div class="nov-cin">
+									<img src="img/mbu1.jpg" width="100%" alt="">
+								</div>
+							</div>
+							<div class="trein centrado-devices">
+								<div class="nov-cin">
+									<img src="img/mbu1.jpg" width="100%" alt="">
+								</div>
+							</div>
+							<div class="trein centrado-devices">
+								<div class="nov-cin">
+									<img src="img/mbu1.jpg" width="100%" alt="">
+								</div>
+							</div>
+						</div>
+						<div class="forma margin-b10 ">
+							<div class="trein centrado-devices">
+								<div class="nov-cin">
+									<img src="img/mbu1.jpg" width="100%" alt="">
+								</div>
+							</div>
+							<div class="trein centrado-devices">
+								<div class="nov-cin">
+									<img src="img/mbu1.jpg" width="100%" alt="">
+								</div>
+							</div>
+							<div class="trein centrado-devices">
+								<div class="nov-cin">
+									<img src="img/mbu1.jpg" width="100%" alt="">
+								</div>
+							</div>
+						</div>
+						<div class="forma margin-b10">
+							<div class="trein centrado-devices">
+								<div class="nov-cin">
+									<img src="img/mbu1.jpg" width="100%" alt="">
+								</div>
+							</div>
+							<div class="trein centrado-devices">
+								<div class="nov-cin">
+									<img src="img/mbu1.jpg" width="100%" alt="">
+								</div>
+							</div>
+							<div class="trein centrado-devices">
+								<div class="nov-cin">
+									<img src="img/mbu1.jpg" width="100%" alt="">
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+				
+			</div>
+		</div>
+
+		<div id="dudas" class="forma padding-b50 padding-t50"> 
+				<div class="contenedor centrado">
+					
+					<h1>Dudas</h1>
+					<div class="cin">
+						<div class="forma">
+							<div class="nov centrado-devices">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto placeat et ipsam ullam, fugiat consectetur id. Inventore tenetur, ratione harum. Esse aut pariatur quisquam quas deserunt temporibus, aperiam itaque laudantium.</p>
+								<p>Omnis dolorum voluptatibus, non veniam error ipsa debitis harum aspernatur qui incidunt dicta, iusto officiis doloribus illum autem eveniet porro officia eum ex id exercitationem fugit! Recusandae nobis, facilis expedita!</p>								
+							</div>
+						</div>
+					</div>
+					<div class="cin">
+						<img src="img/mbu1.jpg" width="100%"></img>
+					</div>
+				
+				</div>
+		</div>
+		<footer>
+			<div class="forma padding-b50 padding-t50"> 
+				<div class="contenedor centrado">
+					<div class="forma">
+						<div class="diez centrado">
+							<a href="index.html">
+								<img src="img/logo-mbu.svg" width="100%" alt="">
+							</a>
+						</div>
+						<div class="txt-center">
+							<h3>Somos unas chicas bien buen pedo</h3>
+							<p>Diseño y maquetación <a href="http://isoldesign.tumblr.com/web" target="_blank">Soy la Xaz</a></p>
+							<small class="small">Todos los derechos reservados</small>
+							<p><a href="https://www.facebook.com/groups/372789066388464/" target="_blank"><img src="img/face.svg" class="social"></a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,15 @@
+<header class="forma menu">
+			<nav class="forma">
+				<div class="vein-cin logotipo left">
+					<a href="#">MBU Zona Centro</a>
+				</div>
+				<div class="set-cin">
+					<div class="cin-menu right menu txt-center">
+						<a href="#nosotras"><img src="img/nosotras.svg" width="25%" class="left"> Nosotras</a>
+						<a href="#galeria"><img src="img/galeria.svg" width="25%" class="left">Galería</a>
+						<a href="#dudas"><img src="img/dudas.svg" width="25%" class="left">Dudas</a>
+						<a href="#"><img src="img/login.svg" width="25%" class="left">Log in</a>
+					</div>
+				</div>
+			</nav>
+		</header>
